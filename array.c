ffcb1ed33cd902604d8a54f7b2fddcc88d17f0ec
Deepak Chaudhary <deepakkumar312005@gmail.com>
2024-10-06 10:13:52 +0530

    Added array

---
+++ array.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a[5];
+//     printf("%d\n", a[0]);
+//     printf("%d\n", a[1]);
+//     printf("%d\n", a[2]);
+//     printf("%d\n", a[3]);
+//     printf("%d\n", a[4]);
+//     return 0;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+//     int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     printf("%d\n", a[0]);
+//     printf("%d\n", a[1]);
+//     printf("%d\n", a[2]);
+//     printf("%d\n", a[3]);
+//     printf("%d\n", a[4]);
+//     printf("%d\n", a[5]);
+//     printf("%d\n", a[6]);
+//     printf("%d\n", a[7]);
+//     printf("%d\n", a[8]);
+//     printf("%d\n", a[9]);
+//     printf("%d\n", a[10]);
+//     return 0;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+//     int a[5] = {1, 2, 3};
+//     printf("%d\n", a[0]);
+//     printf("%d\n", a[1]);
+//     printf("%d\n", a[2]);
+//     printf("%d\n", a[3]);
+//     printf("%d\n", a[4]);
+//     return 0;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+//     int a[5] = {};
+//     printf("%d\n", a[0]);
+//     printf("%d\n", a[1]);
+//     printf("%d\n", a[2]);
+//     printf("%d\n", a[3]);
+//     printf("%d\n", a[4]);
+//     return 0;
+// }
