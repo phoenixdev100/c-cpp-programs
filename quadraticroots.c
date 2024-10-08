9d399ca9fe1e5c7cd3d4db6f16af30a2949b6dbf
Deepak Chaudhary <deepakkumar312005@gmail.com>
2024-10-08 09:10:04 +0530

    Added quadraticroots

---
+++ quadraticroots.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float a, b, c, discriminant, root1, root2, realPart, imagPart;
+    printf("Enter coefficients a (x^2), b (x) and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    discriminant = (b * b) - (4 * a * c);
+
+    // condition for real and different roots
+    if (discriminant > 0)
+    {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("root1 = %f and root2 = %f", root1, root2);
+    }
+
+    // condition for real and equal roots
+    else if (discriminant == 0)
+    {
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %f.", root1);
+    }
+
+    // if roots are not real
+    else
+    {
+        // realPart = -b / (2 * a);
+        // imagPart = sqrt(-discriminant) / (2 * a);
+        printf("Roots are imaginary.");
+        // printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imagPart, realPart, imagPart);
+    }
+
+    return 0;
+}
